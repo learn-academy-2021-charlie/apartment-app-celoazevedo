b5c4d9b31a78cc04c2b5ffaaabc70661718a1a0d
celoazevedo <meazevedo86@gmail.com>
2021-09-01 08:54:50 -0700

    add some style to header component and apartmentindex page.

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, Col, CardSubtitle } from 'reactstrap'
+import { Card, CardTitle, Col, CardSubtitle, CardText, CardBody, CardLink} from 'reactstrap'
 
 class ApartmentIndex extends Component {
     render() {
         return (
-            <div>
+            <div className="cards-container">
                 <h2>Apartment Index Page!</h2>
                 <br />
                 <Col sm="6">
                     { this.props.apartments.map(apartment => {
                     return (
-                        <Card body key={ apartment.id }>
+                        <Card body className="text-center" key={ apartment.id }>
                             <CardTitle>
-                                <h4>{ apartment.street }</h4>
+                                <h4>{ `Location: ${apartment.street}, ${apartment.city}, ${apartment.state}` }</h4>
                             </CardTitle>
                             <CardSubtitle tag="h6" className="mb-2 text-muted">
-                                <p>{ apartment.city }</p>
+                                { `${apartment.bedrooms} bedrooms and ${apartment.bathrooms} baths. Monthly Rent: ${apartment.price}` }
                             </CardSubtitle>
+                            <CardBody>
+                                <CardText>{ `If interested please contact ${apartment.manager} at ${apartment.email}` }</CardText>
+                                <CardLink href="#">See Apt Details</CardLink>
+                            </CardBody>
                         </Card>
                     )
                     })}
