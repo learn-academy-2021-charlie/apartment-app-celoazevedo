433b35b30674c6e099c4cc03d9dcbe30c44be2c2
celoazevedo <meazevedo86@gmail.com>
2021-09-02 15:40:44 -0700

    add not found page, test pass

---
+++ app/javascript/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h3 className="notFoundTitle">Not Found!</h3>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
